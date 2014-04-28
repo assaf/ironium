4036e0cf9c84010bfe95b99f8af010f8f4a0a6ca
Assaf Arkin <assaf@labnotes.org>
2014-04-28 16:21:16 -0700

    Updated npm dependencies, got rid of gulp-exec

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const clean   = require('gulp-clean');
-const exec    = require('gulp-exec');
+const exec    = require('child_process').exec;
+const File    = require('fs');
 const gulp    = require('gulp');
 const notify  = require('gulp-notify');
 const OS      = require('os');
@@ -7,6 +8,7 @@ const Path    = require('path');
 const replace = require('gulp-replace');
 const traceur = require('traceur');
 const through = require('through2');
+const version = require('./package.json').version;
 
 
 // Compile then watch -> compile
@@ -20,11 +22,11 @@ gulp.task('default', function() {
 
 // Compile ES6 in src to ES5 in lib
 gulp.task('build', function() {
-  const options = {
+  var options = {
     sourceMaps:     true,
     modules:        'commonjs'
   };
-  const compile = gulp.src('src/**/*.js')
+  var compile = gulp.src('src/**/*.js')
     .pipe(es6(options))
     .pipe(gulp.dest('lib'));
   // Notifications only available on Mac
@@ -38,14 +40,16 @@ gulp.task('clean', function() {
 });
 
 // Run mocha, used by release task
-gulp.task('test', ['build'], function() {
-  return gulp.src('test').pipe(exec('mocha'));
+gulp.task('test', ['build'], function(callback) {
+  exec('mocha', function(error, stdout) {
+    process.stdout.write(stdout);
+    callback(error);
+  });
 });
 
 
 // Used by gulp release to update element.svg with new version number.
 gulp.task('element', function() {
-  const version = require('./package.json').version;
   return gulp.src('element.svg')
     .pipe(replace(/<tspan id="version">[\d\.]+<\/tspan>/, '<tspan id="version">' + version + '</tspan>'))
     .pipe(gulp.dest('.'));
@@ -54,33 +58,30 @@ gulp.task('element', function() {
 
 // Used by gulp release to create a changelog summary in change.log.
 gulp.task('changelog', function(callback) {
-  const Child = require('child_process');
-  const File  = require('fs');
-
-    
   // Get the most recent tag
-  Child.exec('git describe --abbrev=0 --tags', function(error, stdout) {
-    const tag = stdout.trim();
+  exec('git describe --abbrev=0 --tags', function(error, stdout) {
+    var tag = stdout.trim();
     // Get summary of all commits since that tag
-    Child.exec('git log ' + tag + '..HEAD --pretty=format:%s%n', function(error, stdout) {
-      const log = stdout;
+    exec('git log ' + tag + '..HEAD --pretty=format:%s%n', function(error, stdout) {
+      var log = stdout;
       File.writeFile('change.log', log, 'utf-8', callback);
     });
   });
 });
 
 // Used by npm publish to create a Version N.N commit and tag it.
-gulp.task('tag-release', ['element', 'changelog'], function() {
-  const version = require('./package.json').version;
-  const message = "Release " + version;
-
-  return gulp.src('change.log')
-    .pipe(exec('git add package.json CHANGELOG.md element.svg'))
-    .pipe(exec('git commit --allow-empty -m "' + message + '"'))
-    .pipe(exec('git push origin master'))
-    .pipe(exec('git tag -a ' + version + ' --file change.log'))
-    .pipe(exec('git push origin ' + version))
-    .pipe(clean());
+gulp.task('tag-release', ['element', 'changelog'], function(callback) {
+  var message = "Release " + version;
+  var script  =  "\
+    git add package.json CHANGELOG.md element.svg     &&\
+    git commit --allow-empty -m \"" + message + "\")) &&\
+    git push origin master                            &&\
+    git tag -a " + version + " --file change.log      &&\
+    git push origin " + version;
+  exec(script, function(error, stdout) {
+    process.stdout.write(stdout);
+    callback(error);
+  });
 });
 
 
