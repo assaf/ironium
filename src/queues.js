8c9c6ba0babbf70e2779d29c59543e4502852ac9
Assaf Arkin <assaf@labnotes.org>
2015-06-12 15:08:41 -0700

    Expend message when failing to queue job

--- src/queues.js
+++ src/queues.js
@@ -361,6 +361,9 @@ class Queue extends EventEmitter {
       const jobID = await this._put.request('put', priority, delay, timeToRun, payload)
       this._notify.debug('Queued job %s on queue %s', jobID, this.name, payload);
       return jobID;
+    } catch (error) {
+      error.message = `Error queuing to ${this.name}: ${error.message}`;
+      throw error;
     } finally {
       --this._queuing;
       this.emit('ready');
