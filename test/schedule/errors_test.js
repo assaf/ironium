c224804ef57f8de4a1a712bd0aff3d837e83efe9
Damian Janowski <jano@dimaion.com>
2017-03-16 14:06:45 -0300

    Make sure one-off scheduled jobs are retried.

--- test/schedule/errors_test.js
+++ test/schedule/errors_test.js
@@ -50,3 +50,48 @@ describe('Scheduled job with interval, failing', function() {
     after(TimeKeeper.reset);
   });
 });
+
+
+describe('One-off scheduled job, failing', function() {
+  let count = 0;
+
+  before(setup);
+
+  before(function() {
+    TimeKeeper.travel('2015-06-29T20:00:00Z');
+    Ironium.scheduleJob('one-off', new Date('2015-06-29T21:00:00Z'), function() {
+      // Fails the first time.
+      count++;
+      if (count === 1)
+        return Promise.reject(new Error('Transient'));
+      else
+        return Promise.resolve();
+    });
+  });
+
+  describe('when due', function() {
+    before(function() {
+      TimeKeeper.travel(new Date('2015-06-29T21:01:00Z'));
+    });
+    before(function() {
+      return Ironium.runOnce().catch(function() {})
+    });
+
+    it('should have run scheduled job once', function() {
+      assert.equal(count, 1);
+    });
+
+    describe('after a while', function() {
+      before(function() {
+        TimeKeeper.travel(Date.now() + ms('5m'));
+      });
+      before(Ironium.runOnce);
+
+      it('should run scheduled job again', function() {
+        assert.equal(count, 2);
+      });
+    });
+
+    after(TimeKeeper.reset);
+  });
+});
