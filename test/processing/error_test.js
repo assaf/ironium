cb1e6ca03394afd841879f3c6d6d0257dad8f59b
Damian Janowski <jano@dimaion.com>
2017-05-02 16:44:36 -0300

    Add support for SQS.

--- test/processing/error_test.js
+++ test/processing/error_test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const assert          = require('assert');
+const Crypto          = require('crypto');
+const getAWSConfig    = require('../aws_config');
 const getIronMQConfig = require('../iron_mq_config');
 const Ironium         = require('../..');
 const setup           = require('../helpers');
@@ -124,3 +126,50 @@ describe('Running a job with errors - IronMQ', function() {
   });
 });
 
+
+(getAWSConfig.isAvailable ? describe : describe.skip)('Running a job with errors - SQS', function() {
+  const randomJobID = Crypto.randomBytes(256).toString('hex');
+  let errorQueue;
+  let runs;
+
+  function countAndFailJob(job) {
+    if (job.id === randomJobID) {
+      runs++;
+      return Promise.reject(new Error('fail'));
+    } else
+      return Promise.resolve();
+  }
+
+  before(setup);
+
+  before(function() {
+    runs = 0;
+  });
+
+  before(function() {
+    Ironium.configure(getAWSConfig());
+  });
+  before(function() {
+    errorQueue = Ironium.queue('error-other');
+    errorQueue.eachJob(countAndFailJob);
+  });
+  before(function() {
+    return errorQueue.queueJob({ id: randomJobID });
+  });
+  before(function(done) {
+    process.env.NODE_ENV = 'production';
+    Ironium.start();
+    setTimeout(done, 3000);
+  });
+
+  it('should return the job back to the queue with a delay', function() {
+    // Only 1 run.
+    assert.equal(runs, 1);
+  });
+
+  after(function() {
+    process.env.NODE_ENV = 'test';
+    Ironium.stop();
+    Ironium.configure({});
+  });
+});
